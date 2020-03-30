f5ca24b19e34b876fa9de171c6e82766703287fe
mikle97pir <mikle97pir@yandex.ru>
2020-03-31 00:23:45 +0300

    removing workflows

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,6 @@
 using Documenter, Microlensing
 
-makedocs(
-    modules = [ Microlensing ],
-    doctest = true,
-    format = :html,
-    sitename = "Microlensing.jl",
-    authors = "Mikhail Pirogov",
-    linkcheck = false,
-    pages = [
-        "Home" => "index.md"
-    ],
-    # Use clean URLs, unless built as a "local" build
-    html_prettyurls = !("local" in ARGS),
-)
-
+makedocs(sitename="Microlensing.jl")
 deploydocs(
-    repo    = "github.com/mikle97pir/mikle97pir.jl.git",
-    target  = "build",
-    deps    = nothing,
-    make    = nothing
+    repo = "github.com/mikle97pir/Microlensing.jl.git",
 )
