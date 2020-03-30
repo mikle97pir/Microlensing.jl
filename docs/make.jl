da2733fe0c080e48a39f0a6207f2526ae65c7edf
mikle97pir <mikle97pir@yandex.ru>
2020-03-31 00:10:05 +0300

    Changed make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,22 @@
 using Documenter, Microlensing
 
-makedocs(sitename="Microlensing.jl")
+makedocs(
+    modules = [ Microlensing ],
+    doctest = true,
+    format = :html,
+    sitename = "Microlensing.jl",
+    authors = "Mikhail Pirogov",
+    linkcheck = false,
+    pages = [
+        "Home" => "index.md"
+    ],
+    # Use clean URLs, unless built as a "local" build
+    html_prettyurls = !("local" in ARGS),
+)
+
 deploydocs(
-    repo = "github.com/mikle97pir/Microlensing.jl.git",
+    repo    = "github.com/mikle97pir/mikle97pir.jl.git",
+    target  = "build",
+    deps    = nothing,
+    make    = nothing
 )
